dc26ef2d729f086f27194d0ba349963b79089a6c
Jaroslav Huss <jaroslavhuss@Jarduv-MacBuk.local>
2024-01-24 12:15:37 +0100

    allowing change FE url by .env

--- apps/client/src/GLOBAL_URL.ts
+++ apps/client/src/GLOBAL_URL.ts
@@ -1,3 +1,2 @@
-//const production = "https://gyno-dotaznik.richtergedeon.biz/api"
-const localhost = "http://localhost:5009/api";
-export const GLOBAL_URL: string = localhost;
+export const GLOBAL_URL: string =
+  import.meta.env.FE_URL || "http://localhost:5009/api";
